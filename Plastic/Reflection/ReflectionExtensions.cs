aa049dfc2fbe54c5cfe27bfd86962159eaa97095
rogerjohansson <rogeralsing@gmail.com>
2020-11-28 09:34:52 +0100

    .

---
+++ Plastic/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Reflection;
+
+namespace PlasticLang.Reflection
+{
+    public static class ReflectionExtensions
+    {
+        public static PropertyInfo GetProperty(this object self, string name)
+        {
+            var prop = self.GetType().GetProperty(name);
+            if (prop is null) throw new NotSupportedException();
+            return prop;
+        }
+        
+        public static dynamic? GetPropertyValue(this object self, string name)
+        {
+            var prop = self.GetType().GetProperty(name);
+            if (prop is null) throw new NotSupportedException();
+            return prop.GetValue(self);
+        }
+    }
+}
